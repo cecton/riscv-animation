0afcb89812318e8b9ddd9359eadc584b4c88fea9
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-10-31 17:21:26 +0100

    Perf optimization

--- src/main.rs
+++ src/main.rs
@@ -98,61 +98,59 @@ fn main() -> ! {
     data[0] = 0b01000000;
 
     // a not-so-small macro to help us draw an image
-    macro_rules! draw_frame {
-        ($frame:expr) => {{
-            // an iterator that will convert the frame's bytes to data bytes usable by the screen:
-            //
-            // every byte sent to the screen draws 2 pixels: the first 4 bits are for the first
-            // pixel while the last 4 bits are for the second pixel
-            //
-            // every byte in the frame contains 8 bits so 8 monochromatic pixels
-            //
-            // 8 / 2 = 4
-            //
-            // this iterator returns 4 data bytes for 1 frame byte
-            let mut chunks = $frame.iter().map(|x| {
-                [
-                    (x & 0b10000000).count_ones() as u8 * 0b11110000
-                        + (x & 0b01000000).count_ones() as u8 * 0b00001111,
-                    (x & 0b00100000).count_ones() as u8 * 0b11110000
-                        + (x & 0b00010000).count_ones() as u8 * 0b00001111,
-                    (x & 0b00001000).count_ones() as u8 * 0b11110000
-                        + (x & 0b00000100).count_ones() as u8 * 0b00001111,
-                    (x & 0b00000010).count_ones() as u8 * 0b11110000
-                        + (x & 0b00000001).count_ones() as u8 * 0b00001111,
-                ]
-            });
-            // we count the number of bytes that have been copied so we don't send the whole buffer
-            let mut i = 1;
-            while let Some(chunk) = chunks.next() {
-                // copy_from_slice requires that the source slice and the destination slice are
-                // exactly the same otherwise it will panic
-                data[i..(i + 4)].copy_from_slice(&chunk);
-                i += 4;
-            }
+    let mut draw_frame = |frame: &[u8]| {
+        // an iterator that will convert the frame's bytes to data bytes usable by the screen:
+        //
+        // every byte sent to the screen draws 2 pixels: the first 4 bits are for the first
+        // pixel while the last 4 bits are for the second pixel
+        //
+        // every byte in the frame contains 8 bits so 8 monochromatic pixels
+        //
+        // 8 / 2 = 4
+        //
+        // this iterator returns 4 data bytes for 1 frame byte
+        let mut chunks = frame.iter().map(|x| {
+            [
+                (x & 0b10000000).count_ones() as u8 * 0b11110000
+                    + (x & 0b01000000).count_ones() as u8 * 0b00001111,
+                (x & 0b00100000).count_ones() as u8 * 0b11110000
+                    + (x & 0b00010000).count_ones() as u8 * 0b00001111,
+                (x & 0b00001000).count_ones() as u8 * 0b11110000
+                    + (x & 0b00000100).count_ones() as u8 * 0b00001111,
+                (x & 0b00000010).count_ones() as u8 * 0b11110000
+                    + (x & 0b00000001).count_ones() as u8 * 0b00001111,
+            ]
+        });
+        // we count the number of bytes that have been copied so we don't send the whole buffer
+        let mut i = 1;
+        while let Some(chunk) = chunks.next() {
+            // copy_from_slice requires that the source slice and the destination slice are
+            // exactly the same otherwise it will panic
+            data[i..(i + 4)].copy_from_slice(&chunk);
+            i += 4;
+        }
 
-            if let Err(err) = i2c.write(address, &data[..i]) {
-                sprintln!("Error: {:?}", err);
-            }
-            let _ = blue.toggle();
-        }};
-    }
+        if let Err(err) = i2c.write(address, &data[..i]) {
+            sprintln!("Error: {:?}", err);
+        }
+        let _ = blue.toggle();
+    };
 
     loop {
-        draw_frame!(FRAME_1);
-        draw_frame!(FRAME_2);
-        draw_frame!(FRAME_3);
-        draw_frame!(FRAME_4);
-        draw_frame!(FRAME_5);
-        draw_frame!(FRAME_6);
-        draw_frame!(FRAME_7);
-        draw_frame!(FRAME_8);
-        draw_frame!(FRAME_9);
-        draw_frame!(FRAME_10);
-        draw_frame!(FRAME_11);
-        draw_frame!(FRAME_12);
-        draw_frame!(FRAME_13);
-        draw_frame!(FRAME_14);
-        draw_frame!(FRAME_15);
+        draw_frame(FRAME_1);
+        draw_frame(FRAME_2);
+        draw_frame(FRAME_3);
+        draw_frame(FRAME_4);
+        draw_frame(FRAME_5);
+        draw_frame(FRAME_6);
+        draw_frame(FRAME_7);
+        draw_frame(FRAME_8);
+        draw_frame(FRAME_9);
+        draw_frame(FRAME_10);
+        draw_frame(FRAME_11);
+        draw_frame(FRAME_12);
+        draw_frame(FRAME_13);
+        draw_frame(FRAME_14);
+        draw_frame(FRAME_15);
     }
 }
